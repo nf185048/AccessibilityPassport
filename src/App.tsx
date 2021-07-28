7be9f9f8c3ca5fe0d0206bf97809a939641519d5
Nishat Firoj <nf185048@ncr.com>
2021-07-28 11:55:40 -0400

    webcam barcoding functionality implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import BarcodeScannerComponent from "react-webcam-barcode-scanner";
+import { Sample } from './pages'
 
 function App() {
+  const [key, setKey] = useState('Not Found');
+  console.log('key from front end', key)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BarcodeScannerComponent
+        width={500}
+        height={500}
+        onUpdate={(err, result) => {
+          if (result) setKey(result.toString())
+        }}
+      />
+      <Sample key={key} />
+
     </div>
   );
 }
